66b2e08c784c7b8c73811a54cb41a558f9a7994f
Adarsh Singh <107500627+Adarshsingh7@users.noreply.github.com>
2023-08-28 23:18:48 +0530

    Create class3conditionalAndLoops.cpp

---
+++ basic of programming-level 1/class3conditionalAndLoops.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num;
+  // cout << "enter a number\n";
+  // cin >> num;
+
+  // 1) checking number is positive or negative or zero
+  // if (num > 0) cout  << "number is positive";
+  // else if (num < 0) cout<< "number is negative";
+  // else cout << "num is zero";
+
+  // 2) checking  number is even or odd
+  // if (num % 2 == 0) cout << "number is even" << endl;
+  // else cout << "num is odd" << endl;
+  // for(int i = 10; i <= 12; i = i + 1) cout << "monika my darling" << endl;
+
+  // 3) LOOPING
+  // for(int i = 0; i < 10; i++) cout << i << endl;
+  // for(int i = 0; i < 10; i =  i + 2) cout << i << endl;
+
+  // for (int i = 0; i < 5; i++) {
+  //   cout << "A"
+  //        << " ";
+  // }
+
+  // for (int i = 0; i < 3; i++) {
+  //   cout << "outer loop: ";
+  //   for (int j = 0; j < 3; j++) {
+  //     cout << " inner loop"
+  //          << " ";
+  //   }
+  //   cout<<endl;
+  // }
+
+  // for (int i = 0; i <= 10; i++) cout << "adarsh"<<endl;
+  // for(int i = 1; i <= 10; i++) cout << "19 x " << i << " =" << 19 * i <<
+  // endl; for (int i = 0; i <= 100; i++) if (i %2 == 0) cout << i << " ";
+
+  // 4) PATTARN PRINITING
+  // i) square star pattarn
+  // for (int i = 0; i < 4; i++) {
+  //   for (int j = 0; j < 4; j++) {
+  //     cout<<"* ";
+  //   }
+  //   cout<<endl;
+  // }
+
+  // ii) rectangle loop
+  // for (int i = 0; i < 3; i++) {
+  //   for(int j = 0; j < 5; j++) {
+  //     cout<<"* ";
+  //   }
+  //   cout<<endl;
+  // }
+
+  // 3) hollow rectangle
+  // for (int i = 0; i < 5; i++) {
+  //   for (int j = 0; j < 5; j++) {
+  //     if (i == 0 || i == 4)
+  //       cout << "* ";
+  //     else {
+  //       if(j == 0 || j == 4) cout<< "* ";
+  //       else cout<< "  ";
+  //     }
+  //   }
+  //   cout<<endl;
+  // }
+
+  // 4) half pyramid
+  // for(int i = 0; i < 5; i++) {
+  //   for(int j = 0; j < i + 1; j++) {
+  //     cout<<"* ";
+  //   }
+  //   cout<<endl;
+  // }
+
+  // 5) inverted half pyramid
+  // for(int i = 0; i < 5; i++) {
+  //   for (int j = 5; j > i; j--) {
+  //     cout<<"* ";
+  //   }
+  //   cout<<endl;
+  // }
+
+  // 6) numeric half pyramid
+  // for(int i = 0; i < 5; i++) {
+  //   for(int j = 0; j < i + 1; j++) {
+  //     cout<<j + 1<<" ";
+  //   }
+  //   cout<<endl;
+  // }
+}
